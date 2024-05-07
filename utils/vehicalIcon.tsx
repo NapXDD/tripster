eeaaa7a3c7b5c3f049631f39aaf5a987b7f89fc9
NapXDD <khoadole21@gmail.com>
2024-05-08 01:13:02 +0700

    add transportation icon

---
+++ utils/vehicalIcon.tsx
@@ -0,0 +1,21 @@
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import DirectionsBusIcon from "@mui/icons-material/DirectionsBus";
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+
+export const icon = [
+  {
+    title: "Ô tô",
+    icon: <DirectionsCarIcon />,
+    value: "car",
+  },
+  {
+    title: "Xe buýt",
+    icon: <DirectionsBusIcon />,
+    value: "bus",
+  },
+  {
+    title: "Máy bay",
+    icon: <AirplanemodeActiveIcon />,
+    value: "plane",
+  },
+];
