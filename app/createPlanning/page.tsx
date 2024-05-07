81f8da68ec756805e773fb3e74a273f51fe8c89a
NapXDD <khoadole21@gmail.com>
2024-05-08 01:12:12 +0700

    create create plan route

---
+++ app/createPlanning/page.tsx
@@ -0,0 +1,29 @@
+import BudgetInput from "../components/input/budgetInput";
+import DestinationInput from "../components/input/destinationInput";
+import TimeInput from "../components/input/timeInput";
+import TransportationInput from "../components/input/transportationInput";
+
+export default function CreatePlanning() {
+  return (
+    <div className="flex justify-center items-cente">
+      <div className="w-[80%] bg-gray-200 p-5 rounded-lg flex flex-col gap-2">
+        <div className="flex flex-col">
+          <div className="font-bold mb-1">Chọn phương tiện</div>
+          <TransportationInput />
+        </div>
+        <div className="flex flex-col">
+          <div className="font-bold mb-1">Ngân sách</div>
+          <BudgetInput />
+        </div>
+        <div className="flex flex-col">
+          <div className="font-bold mb-1">Thời gian</div>
+          <TimeInput />
+        </div>
+        <div className="flex flex-col">
+          <div className="font-bold mb-1">Điểm đến</div>
+          <DestinationInput />
+        </div>
+      </div>
+    </div>
+  );
+}
