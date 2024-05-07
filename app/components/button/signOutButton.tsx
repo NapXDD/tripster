9cbe6a64a76408cd6f88d3f56c5b30ec4f489621
NapXDD <khoadole21@gmail.com>
2024-05-08 01:11:24 +0700

    handle signout button

--- app/components/button/signOutButton.tsx
+++ app/components/button/signOutButton.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { removeUser } from "@/lib/features/user";
+import { useAppDispatch } from "@/lib/hooks";
 import { signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function SignOutButton() {
+  const dispatch = useAppDispatch();
+
   const handleSignOut = () => {
+    dispatch(removeUser());
     signOut();
+    redirect("/");
   };
   return <button onClick={handleSignOut}>Sign out</button>;
 }
