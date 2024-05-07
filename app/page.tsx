a5ed0c92dac868b7180c447719f7bf8f5d3c738e
NapXDD <khoadole21@gmail.com>
2024-05-08 01:10:24 +0700

    main page ready to css

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
-import SignInModal from "./components/Modal/signInModal";
-import SignUpModal from "./components/Modal/signUpModal";
-import Header from "./components/Header/header";
-import OverLay, { Component } from "./components/overlay";
-import { getUserSession } from "@/lib/session";
-
-const components: Component = {
-  signin: <SignInModal />,
-  signup: <SignUpModal />,
-  forgotpassword: <button>forgot password</button>,
-  resetpassword: <button>reset password</button>,
-};
-
 export default async function Home() {
-  const user = await getUserSession();
   return (
-    <main className="bg-gray-100 w-[100vw] h-[100vh] flex">
-      <Header user={user} />
-      <OverLay Component={components} />
+    <main className="flex">
+      <div>This is main page</div>
     </main>
   );
 }
