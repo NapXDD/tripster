a7661c9a0ed3549ec1e7a08b195313151b145395
NapXDD <khoadole21@gmail.com>
2024-05-08 20:07:53 +0700

    change css

--- app/createPlanning/page.tsx
+++ app/createPlanning/page.tsx
@@ -6,7 +6,7 @@ import TransportationInput from "../components/input/transportationInput";
 export default function CreatePlanning() {
   return (
     <div className="flex justify-center items-cente">
-      <div className="w-[80%] bg-gray-200 p-5 rounded-lg flex flex-col gap-2">
+      <div className="w-[60%] bg-gray-200 p-5 rounded-lg flex flex-col gap-2">
         <div className="flex flex-col">
           <div className="font-bold mb-1">Chọn phương tiện</div>
           <TransportationInput />
@@ -15,7 +15,7 @@ export default function CreatePlanning() {
           <div className="font-bold mb-1">Ngân sách</div>
           <BudgetInput />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col static">
           <div className="font-bold mb-1">Thời gian</div>
           <TimeInput />
         </div>
