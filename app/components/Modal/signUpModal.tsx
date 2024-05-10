2d7442d112337cc13adf1b4f11024d5b573da74f
NapXDD <khoadole21@gmail.com>
2024-05-10 22:59:18 +0700

    refactor modal

--- app/components/Modal/signUpModal.tsx
+++ app/components/Modal/signUpModal.tsx
@@ -1,24 +1,19 @@
 import { FacebookAuthButton, GoogleAuthButton } from "../button/authButton";
-import CloseButton from "../button/closeButton";
 import CredentialButton from "../button/credentialButton";
+import OverlayModal from "./overlayModal";
 
 export default function SignUpModal() {
   return (
-    <div className="bg-white rounded-xl w-full md:w-auto lg:w-auto">
-      <div className="flex m-2 justify-end">
-        <CloseButton />
+    <OverlayModal>
+      <span className="mb-5 text-lg font-bold">Sign up to Tripster</span>
+      <GoogleAuthButton />
+      <FacebookAuthButton />
+      <div className="flex items-center justify-between w-full mt-4 mb-4">
+        <div className="bg-gray-300 w-full h-[1px] mr-5"></div>
+        <div>or</div>
+        <div className="bg-gray-300 w-full h-[1px] ml-5"></div>
       </div>
-      <div className="mb-14 ml-14 mr-14 mt-8 flex flex-col gap-2 justify-center items-center">
-        <span className="mb-5 text-lg font-bold">Sign up to Tripster</span>
-        <GoogleAuthButton />
-        <FacebookAuthButton />
-        <div className="flex items-center justify-between w-full mt-4 mb-4">
-          <div className="bg-gray-300 w-full h-[1px] mr-5"></div>
-          <div>or</div>
-          <div className="bg-gray-300 w-full h-[1px] ml-5"></div>
-        </div>
-        <CredentialButton type="signup" />
-      </div>
-    </div>
+      <CredentialButton type="signup" />
+    </OverlayModal>
   );
 }
