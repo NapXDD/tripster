3a9453c4aa23646aed52226e6d241a9af4edec67
NapXDD <khoadole21@gmail.com>
2024-05-10 22:59:55 +0700

    update overlay component

--- utils/overlayComponent.tsx
+++ utils/overlayComponent.tsx
@@ -1,3 +1,5 @@
+import EditProfileModal from "@/app/components/Modal/editProfileModal";
+import ForgotPasswordModal from "@/app/components/Modal/forgotPasswordModal";
 import SignInModal from "@/app/components/Modal/signInModal";
 import SignUpModal from "@/app/components/Modal/signUpModal";
 import { OverlayComponent } from "@/app/components/overlay";
@@ -5,6 +7,7 @@ import { OverlayComponent } from "@/app/components/overlay";
 export const components: OverlayComponent = {
   signin: <SignInModal />,
   signup: <SignUpModal />,
-  forgotpassword: <button>forgot password</button>,
+  editprofile: <EditProfileModal />,
+  forgotpassword: <ForgotPasswordModal />,
   resetpassword: <button>reset password</button>,
 };
