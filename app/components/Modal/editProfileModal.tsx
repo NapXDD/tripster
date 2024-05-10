41fc0bafd34a5cd95fc89b0450d9d9c260d8fa0d
NapXDD <khoadole21@gmail.com>
2024-05-10 22:58:01 +0700

    create modal

---
+++ app/components/Modal/editProfileModal.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import OverlayModal from "./overlayModal";
+import { avatar } from "@/utils/importer";
+import { EditOutlined } from "@ant-design/icons";
+
+export default function EditProfileModal() {
+  return (
+    <OverlayModal title="Edit profile">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex aspect-square rounded-full justify-center w-[30%] relative">
+          <Image
+            src={avatar}
+            alt="avatar"
+            width={200}
+            height={200}
+            style={{
+              width: "100%",
+              objectFit: "cover",
+              maxWidth: "none",
+              borderRadius: "50%",
+            }}
+          />
+          <button
+            className="bgRed w-[30%] h-[30%] absolute rounded-full
+           bottom-0 right-0 text-white flex justify-center 
+           items-center hover:bgRedHover"
+          >
+            <EditOutlined />
+          </button>
+        </div>
+      </div>
+    </OverlayModal>
+  );
+}
---
+++ app/components/Modal/forgotPasswordModal.tsx
@@ -0,0 +1,9 @@
+import OverlayModal from "./overlayModal";
+
+export default function ForgotPasswordModal() {
+  return (
+    <OverlayModal>
+      <div>this is forgot password modal</div>
+    </OverlayModal>
+  );
+}
