3cc3bb120016d59201c5b052f6d7444ab2f01570
NapXDD <khoadole21@gmail.com>
2024-05-10 17:09:13 +0700

    add avatar to test profile card

--- utils/importer.ts
+++ utils/importer.ts
@@ -1,6 +1,27 @@
+import { Tag } from "@/app/type/tag";
+
 export const googleLogo = "/thirdPartyLogo/Google.png";
 export const facebookLogo = "/thirdPartyLogo/Facebook.png";
+export const avatar = "/avatar/avatar.jpg";
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
 export const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID!;
 export const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET!;
+
+export const activitiesTags: Tag[] = [
+  { title: "act 1", value: "act1" },
+  { title: "act 2", value: "act2" },
+  { title: "act 3", value: "act3" },
+  { title: "act 4", value: "act4" },
+  { title: "act 5", value: "act5" },
+  { title: "act 6", value: "act6" },
+];
+
+export const amentitiesTags: Tag[] = [
+  { title: "ame 1", value: "ame1" },
+  { title: "ame 2", value: "ame2" },
+  { title: "ame 3", value: "ame3" },
+  { title: "ame 4", value: "ame4" },
+  { title: "ame 5", value: "ame5" },
+  { title: "ame 6", value: "ame6" },
+];
