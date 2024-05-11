650a11cbc2f72021849d121eb9d979ca82ae9af9
NapXDD <khoadole21@gmail.com>
2024-05-11 18:37:20 +0700

    refactor code

--- app/components/button/signInButton.tsx
+++ app/components/button/signInButton.tsx
@@ -3,6 +3,7 @@
 import { openModal } from "@/lib/features/modal";
 import { openOverLay } from "@/lib/features/overlay";
 import { useAppDispatch } from "@/lib/hooks";
+import Button from "./button";
 
 export default function SignInButton() {
   const dispatch = useAppDispatch();
@@ -11,11 +12,8 @@ export default function SignInButton() {
     dispatch(openModal("signin"));
   };
   return (
-    <button
-      className="bgRed text-white hover:bgRedHover rounded-full px-5 py-2"
-      onClick={handleOpenSignInModal}
-    >
+    <Button className="px-5 py-2" onClick={handleOpenSignInModal}>
       Sign In
-    </button>
+    </Button>
   );
 }
