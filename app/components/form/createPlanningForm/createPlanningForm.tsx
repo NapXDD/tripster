6a4c30e2d68ac8680e1e3542e3833ed1ed105e23
NapXDD <khoadole21@gmail.com>
2024-05-11 18:38:50 +0700

    create planning form

---
+++ app/components/form/createPlanningForm/createPlanningForm.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import BudgetInput from "../../input/createPlanInput/budgetInput";
+import DestinationInput from "../../input/createPlanInput/destinationInput";
+import SelectTagInput from "../../input/createPlanInput/selectTagInput";
+import TimeInput from "../../input/createPlanInput/timeInput";
+import TransportationInput from "../../input/createPlanInput/transportationInput";
+import { activitiesTags, amentitiesTags } from "@/utils/importer";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import FormItem from "./FormItem";
+import { openOverLay } from "@/lib/features/overlay";
+import { openModal } from "@/lib/features/modal";
+import Button from "../../button/button";
+import { useRouter } from "next/navigation";
+
+export default function CreatePlanningForm() {
+  const planning = useAppSelector((state) => state.planning.value);
+  const user = useAppSelector((state) => state.user.value.user);
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = () => {
+    if (user.email !== "") {
+      console.log(planning);
+      router.push("/planningDetail/1");
+    } else {
+      dispatch(openOverLay(true));
+      dispatch(openModal("signin"));
+    }
+  };
+  return (
+    <div className="w-[60%] bg-gray-200 p-5 rounded-lg flex flex-col gap-2">
+      <FormItem label="Chọn phương tiện">
+        <TransportationInput />
+      </FormItem>
+      <FormItem label="Điểm đến">
+        <DestinationInput />
+      </FormItem>
+      <FormItem label="Ngân sách">
+        <BudgetInput />
+      </FormItem>
+      <FormItem label="Thời gian">
+        <TimeInput />
+      </FormItem>
+      <FormItem label="Hoạt động">
+        <SelectTagInput data={activitiesTags} type="activities" />
+      </FormItem>
+      <FormItem label="Tiện ích, dịch vụ">
+        <SelectTagInput data={amentitiesTags} type="amentities" />
+      </FormItem>
+      <Button className="w-auto px-2 py-2 lg:w-[25%]" onClick={handleSubmit}>
+        Lập kế hoạch
+      </Button>
+    </div>
+  );
+}
