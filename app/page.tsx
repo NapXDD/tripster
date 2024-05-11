8634913c04c784c7c3556dc8ef4df7a3fae02467
NapXDD <khoadole21@gmail.com>
2024-05-11 18:35:39 +0700

    handle main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import { getUserSession } from "@/lib/session";
 import Header from "./components/Header/header";
+import Link from "next/link";
+import Footer from "./components/footer";
 
 export default async function Home() {
   const user = await getUserSession();
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col h-[100vh] justify-between">
       <Header user={user} />
-      <main className="flex justify-center">
-        <div className="w-[80%]">This is main page</div>
+      <main className="flex justify-center w-full">
+        <div className="w-[80%] min-h-[calc(100vh-10rem)] flex items-center">
+          <div className="flex flex-col">
+            <div>lmao</div>
+            <Link
+              className="bgRed px-5 py-2 text-white rounded-3xl"
+              href={"/createPlanning"}
+            >
+              Lập kế hoạch
+            </Link>
+          </div>
+        </div>
       </main>
+      <Footer />
     </div>
   );
 }
