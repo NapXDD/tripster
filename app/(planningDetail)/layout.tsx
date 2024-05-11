64ac4903c8083d806f607e9027bfa38e7840d142
NapXDD <khoadole21@gmail.com>
2024-05-11 18:36:26 +0700

    create planning detail layout

---
+++ app/(planningDetail)/layout.tsx
@@ -0,0 +1,30 @@
+import { getUserSession } from "@/lib/session";
+import StoreProvider from "../StoreProvider";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import OverLay from "../components/overlay";
+import Header from "../components/Header/header";
+import { components } from "@/utils/overlayComponent";
+import SideBar from "../components/sidebar";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const user = await getUserSession();
+  return (
+    <StoreProvider>
+      <AntdRegistry>
+        <OverLay Component={components} />
+
+        <div className="flex flex-col min-h-screen">
+          <div className="fixed w-full bg-white">
+            <Header user={user} />
+          </div>
+          <SideBar />
+          <div className="flex min-h-[calc(100vh-10rem)]">{children}</div>
+        </div>
+      </AntdRegistry>
+    </StoreProvider>
+  );
+}
