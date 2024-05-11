333b355423ee48e9839e7f2c92722f74ea448a8b
NapXDD <khoadole21@gmail.com>
2024-05-11 18:37:42 +0700

    create card for planning detail page

---
+++ app/components/card/budgetCard.tsx
@@ -0,0 +1,13 @@
+import PlanningDetailLayout from "./planningDetailLayout";
+
+export default function BudgetCard() {
+  return (
+    <PlanningDetailLayout>
+      <div className="h-32">
+        <div className="text-gray-800 font-bold">
+          <div className="pt-2">Budget</div>
+        </div>
+      </div>
+    </PlanningDetailLayout>
+  );
+}
