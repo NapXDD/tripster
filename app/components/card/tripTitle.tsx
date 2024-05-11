219daacfe1740a5c313a1359314257f5d6ae62f2
NapXDD <khoadole21@gmail.com>
2024-05-11 18:38:07 +0700

    create trip title card

---
+++ app/components/card/tripTitle.tsx
@@ -0,0 +1,11 @@
+import PlanningDetailLayout from "./planningDetailLayout";
+
+export default function TripTitle() {
+  return (
+    <PlanningDetailLayout>
+      <div className="font-bold h-16 flex items-center text-gray-800">
+        Trip to HCM
+      </div>
+    </PlanningDetailLayout>
+  );
+}
