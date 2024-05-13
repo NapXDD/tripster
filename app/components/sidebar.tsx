62ed1edb02f71f468a0c67a9970107b6ed0a93ff
NapXDD <khoadole21@gmail.com>
2024-05-13 18:32:55 +0700

    handle sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,5 +1,13 @@
+import BudgetList from "./list/sidebar/budgetList";
+import InteraryList from "./list/sidebar/interaryList";
+
 export default function SideBar() {
   return (
-    <div className="fixed w-[25%] mt-[5rem] border h-[calc(100vh-5rem)] rounded-xl lg:w-[15%]"></div>
+    <div className="overflow-y-auto hideScrollBar bg-white fixed w-[25%] mt-[5rem] border h-[calc(100vh-5rem)] lg:w-[15%]">
+      <div className="flex flex-col m-2 mt-10">
+        <InteraryList />
+        <BudgetList />
+      </div>
+    </div>
   );
 }
