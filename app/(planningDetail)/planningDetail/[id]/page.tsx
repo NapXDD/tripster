83f162e20a53ddb67b7aa5f7b45d7eb88a84eb80
NapXDD <khoadole21@gmail.com>
2024-05-14 00:46:52 +0700

    refactor layout

--- app/(planningDetail)/planningDetail/[id]/page.tsx
+++ app/(planningDetail)/planningDetail/[id]/page.tsx
@@ -1,15 +1,13 @@
 import BudgetCard from "@/app/components/card/budgetCard";
 import ItineraryCard from "@/app/components/card/itineraryCard";
 import TripTitle from "@/app/components/card/tripTitle";
-import Footer from "@/app/components/footer";
 
 export default function PlanningDetail({ params }: { params: { id: string } }) {
   return (
-    <div className="w-[75%] mt-[5rem] bg-gray-200 ml-[25%] lg:w-[85%] lg:ml-[15%]">
+    <>
       <TripTitle title="HCM" />
       <ItineraryCard />
       <BudgetCard />
-      <Footer />
-    </div>
+    </>
   );
 }
---
+++ app/(planningDetail)/planningSelection/page.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/app/components/footer";
+import PlanningDetailTab from "@/app/components/tab/planningDetailTab";
+
+export default function PlanningSelection({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <PlanningDetailTab />
+    </>
+  );
+}
