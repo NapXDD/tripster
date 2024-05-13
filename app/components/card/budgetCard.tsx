05f85b688564b772a88a6da25d554ea707398b83
NapXDD <khoadole21@gmail.com>
2024-05-14 00:47:04 +0700

    change component name

--- app/components/card/budgetCard.tsx
+++ app/components/card/budgetCard.tsx
@@ -1,13 +1,13 @@
-import PlanningDetailLayout from "./planningDetailLayout";
+import PlanningDetailContentLayout from "../layout/planningDetailContentLayout";
 
 export default function BudgetCard() {
   return (
-    <PlanningDetailLayout>
+    <PlanningDetailContentLayout>
       <div className="h-32" id="budget">
         <div className="title">
           <div className="pt-2">Budget</div>
         </div>
       </div>
-    </PlanningDetailLayout>
+    </PlanningDetailContentLayout>
   );
 }
