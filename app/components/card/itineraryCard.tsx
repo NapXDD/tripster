6fbb8cee0a9913e0bb460afcb121e05a1c41a9d5
NapXDD <khoadole21@gmail.com>
2024-05-14 17:23:59 +0700

    change css

--- app/components/card/itineraryCard.tsx
+++ app/components/card/itineraryCard.tsx
@@ -1,12 +1,12 @@
 import ItineraryListCard from "../list/itineraryListCard";
 import PlanningDetailContentLayout from "../layout/planningDetailContentLayout";
 
-export default function ItineraryCard() {
+export default function ItineraryCard({ id }: { id?: string }) {
   return (
     <PlanningDetailContentLayout>
-      <div className="h-screen" id="itinerary">
+      <div className="h-auto pb-8" id="itinerary">
         <div className="title">
-          <div className="pt-2">Itinerary</div>
+          <div className="pt-8 text-2xl pb-4">Lịch trình</div>
         </div>
         <ItineraryListCard />
       </div>
