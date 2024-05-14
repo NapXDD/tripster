7c12765b14b1c05af524e4dc0a36b204f7979982
NapXDD <khoadole21@gmail.com>
2024-05-14 17:26:11 +0700

    add feature to layout

--- app/components/list/sidebar/listLayout.tsx
+++ app/components/list/sidebar/listLayout.tsx
@@ -4,11 +4,13 @@ import React, { useState } from "react";
 import RotateButton from "../../button/rotateButton";
 
 export default function ListLayout({
-  title,
+  id,
   children,
+  title,
 }: {
-  title: string;
+  id: string;
   children?: React.ReactNode;
+  title: string;
 }) {
   const [visible, setVisible] = useState(false);
 
@@ -20,7 +22,7 @@ export default function ListLayout({
     <div className="flex flex-col title mb-10">
       <div className="flex gap-2">
         <RotateButton handleVisible={handleVisible} />
-        <a href={`#${title.toLocaleLowerCase()}`}>{title}</a>
+        <a href={`#${id.toLocaleLowerCase()}`}>{title}</a>
       </div>
       {visible ? (
         <div className="ml-[calc(16px+0.5rem)]">{children}</div>
