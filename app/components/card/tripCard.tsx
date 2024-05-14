1ca8b74c367434718350be83d875b205142e9e9d
NapXDD <khoadole21@gmail.com>
2024-05-14 17:25:22 +0700

    add feature to card

--- app/components/card/tripCard.tsx
+++ app/components/card/tripCard.tsx
@@ -5,10 +5,18 @@ import Image from "next/image";
 import DropDownButton from "../button/dropDownButton";
 import Link from "next/link";
 
-export default function TripCard({ id }: { id: string }) {
+export default function TripCard({
+  id,
+  noDropDown,
+}: {
+  id: string;
+  noDropDown?: boolean;
+}) {
   return (
     <div className="flex flex-col gap-2 relative">
-      <DropDownButton id={id} className="right-0 top-0 mr-2 mt-2 absolute" />
+      {!noDropDown ? (
+        <DropDownButton id={id} className="right-0 top-0 mr-2 mt-2 absolute" />
+      ) : null}
       <Link href={`/planningDetail/${id}`}>
         <div className="flex flex-col justify-end">
           <Image
@@ -19,8 +27,8 @@ export default function TripCard({ id }: { id: string }) {
             className="w-full rounded-lg"
             objectFit="cover"
           />
-          <div>Trip to HCM</div>
-          <div className="text-gray-600">Apr1-May7</div>
+          <div className="text-sm">Chuyến đi tới HCM</div>
+          <div className="text-gray-600 text-sm">Apr1-May7</div>
         </div>
       </Link>
     </div>
