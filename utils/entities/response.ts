4e1b482b7ec004a629521ba57829883880cab4d3
NapXDD <khoadole21@gmail.com>
2024-05-15 12:10:36 +0700

    add entities and dto

---
+++ utils/entities/response.ts
@@ -0,0 +1,8 @@
+export default interface resposneAPI<T> {
+  message: string;
+  status: string;
+  messageData: T
+}
+
+
+
---
+++ utils/entities/signup.ts
@@ -0,0 +1,6 @@
+export interface SignUpEntity {
+  user: {
+    fullname: string;
+    email: string;
+  };
+}
