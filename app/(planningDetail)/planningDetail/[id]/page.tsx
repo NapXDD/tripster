19c9c4f835c3540b4416b97a5eb35aec80ce6dae
NapXDD <khoadole21@gmail.com>
2024-05-15 12:03:43 +0700

    testing api

--- app/(planningDetail)/planningDetail/[id]/page.tsx
+++ app/(planningDetail)/planningDetail/[id]/page.tsx
@@ -1,6 +1,11 @@
 import BudgetCard from "@/app/components/card/budgetCard";
 import ItineraryCard from "@/app/components/card/itineraryCard";
 import TripTitle from "@/app/components/card/tripTitle";
+import { BASE_URL } from "@/utils/importer";
+
+async function getPlanningById({id} : {id: string}){
+  const response = await fetch(`${BASE_URL}/`)
+}
 
 export default function PlanningDetail({ params }: { params: { id: string } }) {
   return (
