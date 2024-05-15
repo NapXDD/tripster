b9ef69ad1815df5b91a938b7a0715bdc449572c1
NapXDD <khoadole21@gmail.com>
2024-05-16 00:06:50 +0700

    refactor utilities code

--- app/components/list/sidebar/interaryList.tsx
+++ app/components/list/sidebar/interaryList.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-import { useAppSelector } from "@/lib/hooks";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import ListItem from "./listItem";
 import ListLayout from "./listLayout";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import dayjs from "dayjs";
+import { setListDate } from "@/lib/features/createPlanning";
+import { getListDate } from "@/utils/getListDate";
 
 export default function InteraryList() {
   const pathName = usePathname();
   const router = useRouter();
   const [dates, setDates] = useState<string[]>([]);
+  const dispatch = useAppDispatch();
   const newPlan = useAppSelector((state) => state.createPlanning.value);
   const storedPlan = useAppSelector((state) => state.planning.value);
-
-  const getListDate = (startDate: string, endDate: string): string[] => {
-    let dates: string[] = [];
-    let currentDate = dayjs(startDate, "DD/MM/YYYY");
-    let endD = dayjs(endDate, "DD/MM/YYYY");
-
-    while (currentDate.isBefore(endD) || currentDate.isSame(endD, "day")) {
-      dates.push(currentDate.format("ddd - DD/MM"));
-      currentDate = currentDate.add(1, "day");
-    }
-    return dates;
-  };
 
   useEffect(() => {
     let listDate: string[] = [];
@@ -33,6 +23,7 @@ export default function InteraryList() {
         router.replace("/");
       }
       listDate = getListDate(newPlan.startDate, newPlan.endDate);
+      dispatch(setListDate(listDate));
     } else {
       listDate = getListDate(storedPlan.startDate, storedPlan.endDate);
     }
