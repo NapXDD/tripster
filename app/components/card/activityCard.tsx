d77ec88c79e08dbc1146e13635b8fb3ed5cc5563
NapXDD <khoadole21@gmail.com>
2024-05-16 00:03:55 +0700

    add date to activity card

--- app/components/card/activityCard.tsx
+++ app/components/card/activityCard.tsx
@@ -5,8 +5,12 @@ import RotateButton from "../button/rotateButton";
 
 export default function ActivityCard({
   content,
+  date,
+  keyId,
 }: {
   content: string | React.ReactNode;
+  date: string;
+  keyId: string;
 }) {
   const [visible, setVisible] = useState(true);
 
@@ -15,10 +19,10 @@ export default function ActivityCard({
   };
 
   return (
-    <div className="flex flex-col gap-2">
+    <div id={`itinerary${keyId}`} className="flex flex-col gap-2">
       <div className="flex gap-2">
         <RotateButton handleVisible={handleSetVisible} open />
-        <div className="title">Date-time</div>
+        <div className="title">{date}</div>
       </div>
       {visible ? (
         <div className="ml-[calc(16px+0.5rem)]">{content}</div>
