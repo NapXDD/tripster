4f5f1b133efde9eff0dfbd1f7c5b04769781bfc2
NapXDD <khoadole21@gmail.com>
2024-05-16 00:06:31 +0700

    modify itinerary card

--- app/components/list/itineraryListCard.tsx
+++ app/components/list/itineraryListCard.tsx
@@ -1,18 +1,42 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
 import ActivityCard from "../card/activityCard";
 import DestinationCard from "../card/destinationCard";
+import { useEffect, useState } from "react";
 
-const data = [
-  { value: <DestinationCard data={null} /> },
-  { value: <DestinationCard data={null} /> },
-  { value: <DestinationCard data={null} /> },
-  { value: <DestinationCard data={null} /> },
-];
+interface activityCardData {
+  value: JSX.Element;
+  date: string;
+}
 
 export default function ItineraryListCard({ id }: { id?: string }) {
+  const planNumber = useAppSelector((state) => state.planningSelection.value);
+  const newPlan = useAppSelector((state) => state.createPlanning.value);
+  const [data, setData] = useState<activityCardData[]>([]);
+
+  useEffect(() => {
+    if (id === undefined) {
+      let dataList: activityCardData[] = [];
+      newPlan.listDate.forEach((date) => {
+        let data = { value: <DestinationCard data={null} />, date: date };
+        dataList.push(data);
+      });
+      setData(dataList);
+    } else {
+      console.log(id);
+    }
+  }, [newPlan, planNumber]);
+
   return (
     <div className="flex flex-col gap-8">
       {data.map((item, index) => (
-        <ActivityCard key={index + "itinerary"} content={item.value} />
+        <ActivityCard
+          key={index + "itinerary"}
+          content={item.value}
+          date={data[index].date}
+          keyId={index.toString()}
+        />
       ))}
     </div>
   );
