067151745a01c2c45116a621a7c5d488aa756de4
NapXDD <khoadole21@gmail.com>
2024-05-15 12:07:34 +0700

    handle set date for plan

--- app/components/input/createPlanInput/timeInput.tsx
+++ app/components/input/createPlanInput/timeInput.tsx
@@ -1,11 +1,25 @@
 "use client";
+import { setDate } from "@/lib/features/createPlanning";
+import { useAppDispatch } from "@/lib/hooks";
 import { DatePicker } from "antd";
+import { RangePickerProps } from "antd/es/date-picker";
 
 const { RangePicker } = DatePicker;
 
 export default function TimeInput() {
-  const handleValue = (dates: [unknown, unknown] | null) => {
-    console.log(dates);
+  const dispatch = useAppDispatch();
+
+  const handleValue: RangePickerProps["onChange"] = (dates) => {
+    if (dates) {
+      if (dates[0] && dates[1]) {
+        dispatch(
+          setDate([
+            dates[0].format("DD/MM/YYYY"),
+            dates[1].format("DD/MM/YYYY"),
+          ])
+        );
+      }
+    }
   };
 
   return <RangePicker onChange={handleValue} allowClear={false} />;
