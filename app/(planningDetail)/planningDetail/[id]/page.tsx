c293e0ffde20a222ebaeb6ab04649800fac3e545
NapXDD <khoadole21@gmail.com>
2024-05-16 00:02:50 +0700

    add api get plan detail test

--- app/(planningDetail)/planningDetail/[id]/page.tsx
+++ app/(planningDetail)/planningDetail/[id]/page.tsx
@@ -3,15 +3,15 @@ import ItineraryCard from "@/app/components/card/itineraryCard";
 import TripTitle from "@/app/components/card/tripTitle";
 import { BASE_URL } from "@/utils/importer";
 
-async function getPlanningById({id} : {id: string}){
-  const response = await fetch(`${BASE_URL}/`)
+async function getPlanningById({ id }: { id: string }) {
+  const response = await fetch(`${BASE_URL}/planningDetail/${id}`);
 }
 
 export default function PlanningDetail({ params }: { params: { id: string } }) {
   return (
     <>
       <TripTitle title="HCM" />
-      <ItineraryCard id={params.id}/>
+      <ItineraryCard id={params.id} />
       <BudgetCard />
     </>
   );
