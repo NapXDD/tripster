b615a6f5b3f41ac13831f36066903f27657b2ad0
NapXDD <khoadole21@gmail.com>
2024-05-16 00:07:24 +0700

    add css to overlay

--- app/components/Modal/overlayModal.tsx
+++ app/components/Modal/overlayModal.tsx
@@ -8,7 +8,7 @@ export default function OverlayModal({
   title?: string;
 }) {
   return (
-    <div className="bg-white rounded-xl w-full md:w-auto lg:w-auto">
+    <div className="min-w[200px] bg-white rounded-xl w-full md:w-auto lg:w-auto">
       {title ? (
         <div className="flex mt-3 justify-between items-center relative w-full">
           <div className="absolute right-0 mr-2">
@@ -22,7 +22,7 @@ export default function OverlayModal({
         </div>
       )}
 
-      <div className="mb-14 ml-14 mr-14 mt-6 flex flex-col gap-2 justify-center items-center">
+      <div className="mb-6 ml-14 mr-14 mt-6 flex flex-col gap-2 justify-center items-center">
         {children}
       </div>
     </div>
