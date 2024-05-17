8466f7e8bb5f3d04d37b4095a898e6c1f2e1a269
NapXDD <khoadole21@gmail.com>
2024-05-18 01:22:41 +0700

    refactor utility function

--- utils/function/getProvinceList.ts
+++ utils/function/getProvinceList.ts
@@ -1,13 +1,3 @@
-import { destination } from "@/app/type/destination";
-
-export default function removeProvincePrefix(data: destination[]) {
-  let destination: destination[] = [];
-  //remove prefix
-  data.forEach((item) => {
-    destination.push({
-      idProvince: item.idProvince,
-      name: item.name.replace(/^(Thành phố|Tỉnh)\s*/, ""),
-    });
-  });
-  return destination;
+export default function removeProvincePrefix(data: string) {
+  return data.replace(/^(Thành phố|Tỉnh)\s*/, "");
 }
