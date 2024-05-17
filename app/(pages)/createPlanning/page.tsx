e9caa553203a75b8004e178aa7f362387cfa0890
NapXDD <khoadole21@gmail.com>
2024-05-17 09:10:27 +0700

    add province api

--- app/(pages)/createPlanning/page.tsx
+++ app/(pages)/createPlanning/page.tsx
@@ -1,12 +1,22 @@
+import get from "@/utils/api/getAPI";
 import CreatePlanningForm from "../../components/form/createPlanningForm/createPlanningForm";
+import { destination } from "@/app/type/destination";
 
-export default function CreatePlanning() {
+async function getAllProvince() {
+  const response = await get<destination[]>(
+    "https://vietnam-administrative-division-json-server-swart.vercel.app/province"
+  );
+  return response;
+}
+
+export default async function CreatePlanning() {
+  const provinces = await getAllProvince();
   return (
     <div className="flex justify-center items-center flex-col gap-3 mb-4">
       <div className="font-bold text-3xl text-gray-800">
         Tạo kế hoạch cho chuyến đi
       </div>
-      <CreatePlanningForm />
+      <CreatePlanningForm destinationData={provinces} />
     </div>
   );
 }
