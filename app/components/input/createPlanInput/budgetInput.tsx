6434e86596b121fe8c0315e9784381900f779113
NapXDD <khoadole21@gmail.com>
2024-05-18 01:15:59 +0700

    modify budget input

--- app/components/input/createPlanInput/budgetInput.tsx
+++ app/components/input/createPlanInput/budgetInput.tsx
@@ -1,7 +1,40 @@
 "use client";
 
-import { Input } from "antd";
+import { setBudget } from "@/lib/features/createPlanning";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { Input, InputNumber, InputNumberProps, Slider, Space } from "antd";
 
 export default function BudgetInput() {
-  return <Input />;
+  const dispatch = useAppDispatch();
+  const budget = useAppSelector((state) => state.createPlanning.value.budget);
+
+  const onChange: InputNumberProps["onChange"] = (newValue) => {
+    dispatch(setBudget(newValue as number));
+  };
+
+  return (
+    <div className="flex flex-col gap-4 md:lg:flex-row">
+      <Slider
+        className="w-full md:lg:w-[60%]"
+        min={1000000}
+        max={100000000}
+        onChange={onChange}
+        value={budget}
+      />
+      <Space.Compact className="!w-full md:lg:!w-[40%]">
+        <Input
+          style={{ width: "35%", backgroundColor: "white" }}
+          disabled
+          defaultValue="VND"
+        />
+        <InputNumber
+          style={{ width: "65%" }}
+          min={1000000}
+          max={100000000}
+          value={budget}
+          onChange={onChange}
+        />
+      </Space.Compact>
+    </div>
+  );
 }
