36198a0a3ed0c41c1d56ce3ebe0475426ef40210
NapXDD <khoadole21@gmail.com>
2024-05-17 09:17:04 +0700

    add css for logo

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/">
-      <span className="font-bold">Tripster</span>
+      <span className="font-bold text-xl">Tripster</span>
     </Link>
   );
 }
