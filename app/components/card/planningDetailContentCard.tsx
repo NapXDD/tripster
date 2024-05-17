07bede7ac50510c75ef5badfd7cd654aaa847643
NapXDD <khoadole21@gmail.com>
2024-05-18 01:14:48 +0700

    Change lang

--- app/components/card/planningDetailContentCard.tsx
+++ app/components/card/planningDetailContentCard.tsx
@@ -1,16 +1,29 @@
+"use client";
+
 import { Divider } from "antd";
 import BudgetCard from "./budgetCard";
 import ItineraryCard from "./itineraryCard";
 import Button from "../button/button";
+import { useAppDispatch } from "@/lib/hooks";
+import { resetCreatePlanning } from "@/lib/features/createPlanning";
 
 export default function PlanningDetailContentCard() {
+  const dispatch = useAppDispatch();
+
+  const handleSavePlan = () => {
+    //save api here
+    dispatch(resetCreatePlanning());
+  };
+
   return (
     <div>
       <ItineraryCard />
       <Divider />
       <BudgetCard />
       <Divider />
-      <Button className="mb-7 py-1 ml-6">Save</Button>
+      <Button onClick={handleSavePlan} className="mb-7 py-1 ml-6">
+        Lưu
+      </Button>
     </div>
   );
 }
