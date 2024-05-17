4f42911b3b33e359e4bac9d101189034389c75df
NapXDD <khoadole21@gmail.com>
2024-05-18 01:13:20 +0700

    move server action to api folder

--- app/(pages)/createPlanning/page.tsx
+++ app/(pages)/createPlanning/page.tsx
@@ -1,16 +1,8 @@
-import get from "@/utils/api/getAPI";
+import { getAllProvinceOption } from "@/utils/api/province";
 import CreatePlanningForm from "../../components/form/createPlanningForm/createPlanningForm";
-import { destination } from "@/app/type/destination";
-
-async function getAllProvince() {
-  const response = await get<destination[]>(
-    "https://vietnam-administrative-division-json-server-swart.vercel.app/province"
-  );
-  return response;
-}
 
 export default async function CreatePlanning() {
-  const provinces = await getAllProvince();
+  const provinces = await getAllProvinceOption();
   return (
     <div className="flex justify-center items-center flex-col gap-3 mb-4">
       <div className="font-bold text-3xl text-gray-800">
