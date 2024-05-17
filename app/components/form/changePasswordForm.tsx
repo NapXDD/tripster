8528bb2f50632f53b3e0c8e9bbc33ac2f7ea440b
NapXDD <khoadole21@gmail.com>
2024-05-17 09:16:17 +0700

    modify password form

--- app/components/form/changePasswordForm.tsx
+++ app/components/form/changePasswordForm.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { Button, Form, FormProps, Input } from "antd";
+
+type FieldType = {
+  password: string;
+  repassword: string;
+};
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
 export default function ChangePasswordForm() {
-  return <div>this is change password form</div>;
+  return (
+    <Form
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item<FieldType>
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password id="password1" />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        label="ReEnter password"
+        name="repassword"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password id="password2" />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
