c1ca01ac103ad770524ac9ed5b811aed7df69335
NapXDD <khoadole21@gmail.com>
2024-05-18 01:15:42 +0700

    add required ui to Form Item

--- app/components/form/createPlanningForm/FormItem.tsx
+++ app/components/form/createPlanningForm/FormItem.tsx
@@ -1,13 +1,20 @@
 export default function FormItem({
   label,
   children,
+  required,
 }: {
   label: string;
   children: React.ReactNode;
+  required?: boolean;
 }) {
   return (
-    <div className="flex flex-col">
-      <div className="font-semibold mb-1">{label}</div>
+    <div className="flex flex-col gap-2">
+      <div className="font-semibold mb-1">
+        <div className="flex">
+          {required && <span className="text-red-500 mr-2">*</span>}
+          <div className="text-base">{label}</div>
+        </div>
+      </div>
       {children}
     </div>
   );
