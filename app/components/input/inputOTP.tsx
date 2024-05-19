579278e65771c235ed66df873e651b47a029554a
NapXDD <khoadole21@gmail.com>
2024-05-19 20:31:57 +0700

    refactor otp form

--- app/components/input/inputOTP.tsx
+++ app/components/input/inputOTP.tsx
@@ -1,15 +1,49 @@
 "use client";
 
 import { GetProp, Input } from "antd";
-import { OTPProps } from "antd/es/input/OTP";
+import { useRef, useState } from "react";
 
-export default function InputOTP() {
-  const onChange: GetProp<typeof Input.OTP, "onChange"> = (text) => {
-    console.log("onChange:", text);
-  };
-
-  const sharedProps: OTPProps = {
-    onChange,
-  };
-  return <Input.OTP formatter={(str) => str.toUpperCase()} {...sharedProps} />;
+interface PriceInputProps {
+  id?: string;
+  value?: string;
+  onChange?: (value: string) => void;
 }
+
+export const InputOTP: React.FC<PriceInputProps> = (props) => {
+  const { onChange } = props;
+  const [_, setOTP] = useState("");
+  const otpRef = useRef<HTMLDivElement>(null);
+
+  const triggerChange = (changedValue: string) => {
+    onChange?.(changedValue);
+  };
+
+  const onOTPChange: GetProp<typeof Input.OTP, "onKeyUp"> = () => {
+    const otpElement = otpRef?.current;
+    let currentOTP = "";
+    if (otpElement) {
+      const childrenElement = otpElement.children[0].children;
+      const inputElement: HTMLInputElement[] = [];
+      for (let key in childrenElement) {
+        if (typeof childrenElement[key] === "object") {
+          inputElement.push(
+            childrenElement[key] as unknown as HTMLInputElement
+          );
+        }
+      }
+      inputElement.forEach((item) => {
+        if (item.value !== "") {
+          currentOTP = `${currentOTP}${item.value}`;
+        }
+      });
+    }
+    setOTP(currentOTP);
+    triggerChange(currentOTP);
+  };
+
+  return (
+    <div ref={otpRef}>
+      <Input.OTP onKeyUp={onOTPChange} />
+    </div>
+  );
+};
--- utils/validator/checkOTP.ts
+++ utils/validator/checkOTP.ts
@@ -1,16 +1,14 @@
+import { RuleObject } from "antd/es/form";
 import { numberRegex } from "../regex";
 
-export default function checkOTP(otp: string | null) {
-  console.log(otp);
-  if (otp !== null) {
-    if (otp === "") {
-      return Promise.reject(new Error("Nhập OTP là cần thiết"));
-    } else if (otp.length < 6) {
-      return Promise.reject(new Error("Độ dài OTP phải là mã 6 số"));
-    } else if (!numberRegex.test(otp) && otp.length === 6) {
-      return Promise.reject(new Error("OTP phải là số"));
-    } else {
-      return Promise.resolve();
-    }
+export default function checkOTP(_: RuleObject, otp: string) {
+  if (otp === "" || otp === undefined) {
+    return Promise.reject(new Error("OTP là cần thiết"));
+  } else if (otp.length < 6) {
+    return Promise.reject(new Error("Độ dài OTP phải là mã 6 số"));
+  } else if (!numberRegex.test(otp) && otp.length === 6) {
+    return Promise.reject(new Error("OTP phải là số"));
+  } else {
+    return Promise.resolve();
   }
 }
