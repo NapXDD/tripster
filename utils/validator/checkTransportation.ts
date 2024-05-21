ec73094c235c35c13f8b61f4091c88c826ad0eca
NapXDD <khoadole21@gmail.com>
2024-05-21 10:24:08 +0700

    modify funciton

--- utils/validator/checkTransportation.ts
+++ utils/validator/checkTransportation.ts
@@ -1,6 +1,38 @@
-export default function checkTransportation(transportation: string | null) {
-  if (transportation === "") {
-    return Promise.reject(new Error("Phương tiện di chuyển là cần thiết"));
+import { destination } from "@/app/types/destination";
+import { checkValidFlight } from "../api/plan";
+import removeProvincePrefix from "../function/getProvince";
+
+export default function checkTransportation(
+  startPoint: destination,
+  destination: destination,
+  transportation: "coach" | "flight",
+  accessToken: string
+) {
+  console.log("lmao");
+  if (transportation === "flight") {
+    const fetchData = async () => {
+      const res = await checkValidFlight(
+        {
+          departure: removeProvincePrefix(startPoint.name),
+          arrival: removeProvincePrefix(destination.name),
+        },
+        accessToken
+      );
+      if (res.status === "200") {
+        return res.messageData;
+      }
+    };
+    fetchData().then((res) => {
+      if (res) {
+        if (res === true) {
+          return Promise.resolve();
+        } else {
+          return Promise.reject(
+            `Không có chuyến bay từ ${startPoint.name} đến ${destination.name}`
+          );
+        }
+      }
+    });
   } else {
     return Promise.resolve();
   }
