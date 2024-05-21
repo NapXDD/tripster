00535ee7e6e486d8ea7f001c9d0441a6b9a8012a
NapXDD <khoadole21@gmail.com>
2024-05-21 10:02:26 +0700

    modify session funciton

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,4 +1,3 @@
-import { getUserSession } from "@/lib/session";
 import Header from "../components/Header/header";
 import Footer from "../components/footer";
 
@@ -7,16 +6,13 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await getUserSession();
   return (
-    <>
-      <div className="flex flex-col justify-between min-h-screen">
-        <Header user={user} />
-        <div className="flex justify-center min-h-[calc(100vh-10rem)]">
-          <div className="w-[80%] mt-5">{children}</div>
-        </div>
-        <Footer />
+    <div className="flex flex-col justify-between min-h-screen">
+      <Header />
+      <div className="flex justify-center min-h-[calc(100vh-10rem)]">
+        <div className="w-[80%] mt-5">{children}</div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
-import { getUserSession } from "@/lib/session";
 import Header from "./components/Header/header";
 import Footer from "./components/footer";
 import HomePageNotSignIn from "./components/card/homePageNotSignIn";
 import HomePageSignIn from "./components/card/homePageSignIn";
+import { getUserSession } from "@/lib/session";
 
 export default async function Home() {
-  const user = await getUserSession();
-
+  const session = await getUserSession();
   return (
     <div className="flex flex-col h-[100vh] justify-between">
-      <Header user={user} />
+      <Header user={session?.user} />
       <main className="flex justify-center w-full">
         <div className="w-[80%] min-h-[calc(100vh-10rem)] flex items-center">
-          {!user ? <HomePageNotSignIn /> : <HomePageSignIn />}
+          {session?.user === undefined ? (
+            <HomePageNotSignIn />
+          ) : (
+            <HomePageSignIn />
+          )}
         </div>
       </main>
       <Footer />
