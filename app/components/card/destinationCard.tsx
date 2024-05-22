7f434d8b54c4a7d316f2096f8a99106598a5e967
NapXDD <khoadole21@gmail.com>
2024-05-22 10:22:37 +0700

    modify card

--- app/components/card/destinationCard.tsx
+++ app/components/card/destinationCard.tsx
@@ -1,14 +1,48 @@
-export default function DestinationCard({
-  data,
-}: {
-  data: Record<string, string> | null | JSX.Element;
-}) {
+import { activity } from "@/app/types/plan";
+import Image from "next/image";
+
+function ActivityCard({ data }: { data: activity }) {
+  return (
+    <div className="flex gap-4">
+      <div className="flex bg-gray-200 basis-[100%] rounded-lg">
+        <div className="flex flex-col m-4">
+          <div className="title text-lg">{data.name}</div>
+          <div className="text-lg font-semibold text-gray-800">
+            Mở cửa từ {data.openTime} tới {data.closeTime}
+          </div>
+          <div className="text-lg font-semibold text-gray-800">
+            Cost: {data.price} VND
+          </div>
+          <div className="text-lg font-semibold text-gray-800">
+            Rating: {data.rating}
+          </div>
+          <div className="text-lg font-semibold text-gray-800">
+            Hoạt động: {data.type_activity}
+          </div>
+        </div>
+      </div>
+      <Image
+        width={150}
+        height={100}
+        src={data.image}
+        alt={data.name}
+        className="rounded-lg w-auto object-cover"
+      />
+    </div>
+  );
+}
+
+export default function DestinationCard({ data }: { data: activity[] | null }) {
   return (
     <div className="flex flex-col">
       {data === null ? (
         <div className="text-gray-500 text-sm">No place</div>
       ) : (
-        <>Lmao</>
+        <div className="grid grid-flow-row row-span-1 gap-4">
+          {data.map((item, index) => (
+            <ActivityCard key={index + item.name} data={item} />
+          ))}
+        </div>
       )}
     </div>
   );
--- app/components/card/planningDetailContentCard.tsx
+++ app/components/card/planningDetailContentCard.tsx
@@ -4,24 +4,75 @@ import { Divider } from "antd";
 import BudgetCard from "./budgetCard";
 import ItineraryCard from "./itineraryCard";
 import Button from "../button/button";
-import { useAppDispatch } from "@/lib/hooks";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { resetCreatePlanning } from "@/lib/features/createPlanning";
+import { plan } from "@/utils/function/setupPlan";
+import { resetMultiplan } from "@/lib/features/multiplan";
+import HotelCard from "./hotelCard";
+import TransportationCard from "./transportationCard";
+import { resetRawPlan } from "@/lib/features/rawCreatePlan";
+import { savePlan } from "@/utils/api/plan";
+import { SavePlanDTO } from "@/utils/DTO/plan";
+import { useRouter } from "next/navigation";
 
-export default function PlanningDetailContentCard() {
+export default function PlanningDetailContentCard({ data }: { data?: plan }) {
   const dispatch = useAppDispatch();
+  const rawPlanData = useAppSelector(
+    (state) => state.rawPlanData.value.rawPlanData
+  );
+  const multiplan = useAppSelector(
+    (state) => state.multiplanSelect.value.multiplan
+  );
+  const planNumber = useAppSelector(
+    (state) => state.planningSelection.value.planNumber
+  );
+  const budget = useAppSelector((state) => state.createPlanning.value.budget);
+  const token = useAppSelector((state) => state.user.value.user.token);
+  const router = useRouter();
 
-  const handleSavePlan = () => {
+  const handleSavePlan = async () => {
     //save api here
-    dispatch(resetCreatePlanning());
+    if (rawPlanData !== null) {
+      const rawData = rawPlanData.messageData.detailPlans[planNumber];
+      const activitiesId = rawData.activities.map((item) => item.id.toString());
+      const hotel = rawData.hotel.id.toString();
+      const messageData = rawPlanData.messageData;
+      let transport = rawData.transport;
+      const data: SavePlanDTO = {
+        start_day: messageData.start_day,
+        end_day: messageData.end_day,
+        activities: activitiesId,
+        budget: budget.toString(),
+        end_point: messageData.end_point,
+        hotel: hotel,
+        start_point: messageData.start_point,
+        transport: transport,
+      };
+      try {
+        const res = await savePlan(data, token);
+        if (res.status === "200") {
+          router.push(`/planningDetail/${res.messageData.planId}`);
+          dispatch(resetRawPlan());
+          dispatch(resetMultiplan());
+          dispatch(resetCreatePlanning());
+        }
+      } catch (e) {
+        throw e;
+      }
+    }
   };
 
   return (
-    <div>
+    <div className="relative">
       <ItineraryCard />
       <Divider />
-      <BudgetCard />
+      <BudgetCard data={multiplan![planNumber].total} />
       <Divider />
-      <Button onClick={handleSavePlan} className="mb-7 py-1 ml-6">
+      <HotelCard data={multiplan![planNumber].hotel} />
+      <Divider />
+      <TransportationCard data={multiplan![planNumber].transport} />
+      <Divider />
+      <Button type="theme" onClick={handleSavePlan} className="mb-7 py-1 ml-6">
         Lưu
       </Button>
     </div>
