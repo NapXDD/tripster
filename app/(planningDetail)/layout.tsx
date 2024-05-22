31de864e9bc097463a0abe37f4f67c563164d389
NapXDD <khoadole21@gmail.com>
2024-05-22 10:13:49 +0700

    modify layout

--- app/(planningDetail)/layout.tsx
+++ app/(planningDetail)/layout.tsx
@@ -8,16 +8,15 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await getUserSession();
   return (
     <>
       <div className="flex flex-col min-h-screen bg-gray-200">
         <div className="fixed w-full z-50 bg-white">
-          <Header user={user} />
+          <Header />
         </div>
         <SideBar />
         <div className="flex min-h-[calc(100vh-10rem)]">
-          <div className="w-[75%] mt-[5rem] bg-gray-200 ml-[25%] lg:w-[85%] lg:ml-[15%]">
+          <div className="w-[calc(75%-0.5rem)] mt-[5rem] bg-gray-200 ml-[25%] lg:ml-[15%]">
             {children}
             <Footer />
           </div>
