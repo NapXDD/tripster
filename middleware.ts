e378df0dbd0b76e010c8d59c4d864c9f616e1d4f
NapXDD <khoadole21@gmail.com>
2024-05-22 10:12:20 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,19 @@
-import { isAuthenticated } from "./utils/auth";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NEXTAUTH_SECRET } from "./utils/importer";
 
-const protectedRoutes = ["/planning"];
+export const config = {
+  matcher: ["/planningSelection", "/profile/:path*", "/planningDetail/:path*"], // Adjust the matcher to the routes you want to protect
+};
 
-export default function middleware(req: NextRequest) {
-  if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
+export default async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: NEXTAUTH_SECRET });
+  if (!token) {
+    // Redirect to main page if not authenticated
     const absoluteURL = new URL("/", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
+  // Continue to the protected route if authenticated
+  return NextResponse.next();
 }
