056ee2f0afb3b9aa2b40be6e8e784d8ffc48498e
NapXDD <khoadole21@gmail.com>
2024-05-24 21:29:42 +0700

    add config

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,12 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "lh5.googleusercontent.com",
         port: "",
         pathname: "/**",
