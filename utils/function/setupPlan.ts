24a73f96a5c64e87ec24bfa125a182084b9fd0d9
NapXDD <khoadole21@gmail.com>
2024-05-24 21:34:04 +0700

    modify setup plan

--- utils/function/setupPlan.ts
+++ utils/function/setupPlan.ts
@@ -1,9 +1,11 @@
 import dayjs from "dayjs";
-import { CreatePlanEntity } from "../entities/plan";
+import { CreatePlanEntity, PlanDetailEntity } from "../entities/plan";
 import { getListDate } from "./getListDate";
 import { activity, hotel } from "@/app/types/plan";
 import { responseAPI } from "../entities/response";
 import { transport } from "@/app/types/transportation";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 
 type DateRecord = Record<string, activity[]>;
 
@@ -57,5 +59,47 @@ export function setupPlan(data: responseAPI<CreatePlanEntity>) {
     };
     plan.push(data);
   }
+  return plan;
+}
+
+//because of the object difference structure of multiplan and a single plan, so I have to copy the function and modify it
+//to reuse the multiplan reducer
+export function setupSinglePlan(data: responseAPI<PlanDetailEntity>) {
+  const newData = structuredClone(data);
+  const messageData = newData.messageData;
+  const detailPlans = messageData.activity;
+  let startDay = messageData.plan.start_day.split("T")[0];
+  let endDay = messageData.plan.end_day.split("T")[0];
+  startDay = dayjs(startDay, "YYYY-MM-DD").format("DD/MM/YYYY");
+  endDay = dayjs(endDay, "YYYY-MM-DD").format("DD/MM/YYYY");
+  let plansActivities: PlansActivities = [];
+  const listDate = getListDate(startDay, endDay);
+  const length = 1;
+  const dateObject: DateRecord = listDate.reduce(
+    (acc: DateRecord, date: string) => {
+      acc[date] = [];
+      return acc;
+    },
+    {}
+  );
+  const newdetailPlans = structuredClone(detailPlans);
+  for (let i = 0; i < length; i++) {
+    const newDateObject = structuredClone(dateObject);
+    plansActivities[i] = newDateObject;
+  }
+  plansActivities.forEach((item, index) => {
+    let i = index;
+    for (let key in item) {
+      let newActivities = newdetailPlans;
+      const activities = newActivities.splice(0, 3);
+      plansActivities[i][key] = [...activities];
+    }
+  });
+  let plan: plan = {
+    activities: plansActivities[0],
+    hotel: messageData.accommodation[0],
+    total: messageData.plan.budget,
+    transport: messageData.transport[0],
+  };
   return plan;
 }
